63c35c3346a8b98e22b5c250849c1362076a6f47
Luis Antonio Olivera Vasquez <antoliveravasquez11@gmail.com>
2023-05-27 20:21:10 -0500

    Test de eliminar una categoria

--- src/main/java/com/cucumber/categoryservice/repository/CategoryRepository.java
+++ src/main/java/com/cucumber/categoryservice/repository/CategoryRepository.java
@@ -4,6 +4,9 @@ import com.cucumber.categoryservice.document.Category;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends MongoRepository<Category, String> {
+    void deleteCategoryByName(String name);
 }
--- src/test/java/com/cucumber/categoryservice/steps/DeleteCategoryByNameStepDefinitions.java
+++ src/test/java/com/cucumber/categoryservice/steps/DeleteCategoryByNameStepDefinitions.java
@@ -1,22 +1,45 @@
 package com.cucumber.categoryservice.steps;
 
+import com.cucumber.categoryservice.document.Category;
+import com.cucumber.categoryservice.repository.CategoryRepository;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 
 public class DeleteCategoryByNameStepDefinitions {
+    private Category existingCategory;
+    private CategoryRepository categoryRepository;
+    @Before
+    public void setup(){
+        this.categoryRepository = mock(CategoryRepository.class);
+    }
     @Given("existe una categoría para eliminar con nombre {string}")
     public void crearCategoriaExistenteParaEliminar(String nombreCategoria) {
-        // Lógica para crear una categoría existente con el nombre proporcionado
+        this.existingCategory = Category
+                .builder()
+                .id("1")
+                .name(nombreCategoria)
+                .build();
     }
 
     @When("se elimina la categoría {string}")
     public void seEliminaLaCategoría(String arg0) {
+        Mockito.doNothing().when(this.categoryRepository).deleteCategoryByName(arg0);
+        this.categoryRepository.deleteCategoryByName(arg0);
+
     }
 
     @Then("la categoría es eliminada correctamente")
     public void verificarCategoriaEliminada() {
-        // Lógica para verificar que la categoría ha sido eliminada correctamente
+        Mockito.verify(this.categoryRepository, times(1)).deleteById(ArgumentMatchers.any());
     }
 
 
